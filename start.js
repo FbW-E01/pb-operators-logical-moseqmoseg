69eabd2b1b9529efce0baeeabf8d347753d589fc
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-04-12 11:02:56 +0200

    Updated bonus

--- start.js
+++ start.js
@@ -4,12 +4,12 @@ const atoms = 5
 const sandGrains = 10
 const starsInSky = 15
 
-console.log(isDogBetter && isCatBetter) + "this is logic";
-console.log(isDogBetter || isCatBetter);
-console.log(!(isDogBetter && isCatBetter));
-console.log((atoms > starsInSky) && (atoms > sandGrains));
-console.log(atoms !== sandGrains);
-console.log((starsInSky < sandGrains) || (starsInSky > atoms));
-console.log((atoms === starsInSky) || (atoms !== sandGrains));
-console.log((atoms >= 10) && (sandGrains <= 10));
-console.log(((atoms * starsInSky) < 100) || ((atoms * sandGrains) > 100));
+console.log((isDogBetter && isCatBetter) + " The value is false since they have diff values");
+console.log((isDogBetter || isCatBetter) + ` The value is true since only one of them has to be true`);
+console.log((!(isDogBetter && isCatBetter)) + ` The value is true as it is the opposite of the first statement`);
+console.log(((atoms > starsInSky) && (atoms > sandGrains)) + ` The value is false since both results are false`);
+console.log((atoms !== sandGrains) + ` The result is true since atoms is indeed not equal to sandGrains`);
+console.log(((starsInSky < sandGrains) || (starsInSky > atoms)) + ` The result is true because the second statement is true`);
+console.log(((atoms === starsInSky) || (atoms !== sandGrains)) + ` The results is true since the second statement is true`);
+console.log(((atoms >= 10) && (sandGrains <= 10)) + ` The result is false because only one of the results is true `);
+console.log((((atoms * starsInSky) < 100) || ((atoms * sandGrains) > 100)) + ` The result is true since the second statement is true and only one has to be true`);
